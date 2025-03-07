2a071dc2b7fcc1d3b6d87b2e401a929cc5096196
Kontoh <F8877557@fnb.co.za>
2025-03-07 12:14:44 +0000

    debuging

--- src/backend/back.js
+++ src/backend/back.js
@@ -83,3 +83,18 @@ module.exports = {
 
 
 
+//visitor screen//
+
+
+
+const express = require('express');
+const router = express.Router();
+const visitorsController = require('../controllers/visitorsLogsController');
+
+router.get('/by-phone', visitorsController.getVisitorLogsByPhoneNumber);
+router.get('/:id', visitorsController.getVisitorLogById);
+router.post('/', visitorsController.createVisitorLog);
+router.put('/:id', visitorsController.updateVisitorLog);
+router.delete('/:id', visitorsController.deleteVisitorLog);
+
+module.exports = router;
