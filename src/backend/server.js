57e9a2d9a40a30f366c634ff5ccf6bc2e5cdc825
Francis <mobiledev@ikolilu.com>
2025-03-07 15:30:41 +0000

    wereyyy

--- src/backend/server.js
+++ src/backend/server.js
@@ -2,10 +2,20 @@ const express = require('express');
 const router = express.Router();
 const visitorsController = require('../controllers/visitorsLogsController');
 
+// Visitor log routes
+router.get('/', visitorsController.getAllVisitorLogs);
 router.get('/by-phone', visitorsController.getVisitorLogsByPhoneNumber);
 router.get('/:id', visitorsController.getVisitorLogById);
 router.post('/', visitorsController.createVisitorLog);
 router.put('/:id', visitorsController.updateVisitorLog);
 router.delete('/:id', visitorsController.deleteVisitorLog);
+
+// Department routes
+router.get('/departments/all', visitorsController.getAllDepartments);
+router.post('/departments/create', visitorsController.createDepartment);
+
+// Branch routes
+router.get('/branches/all', visitorsController.getAllBranches);
+router.post('/branches/create', visitorsController.createBranch);
 
 module.exports = router;
